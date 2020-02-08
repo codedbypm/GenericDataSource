481692cf9a51fed0854e50db1793377a160167f1
Paolo Moroni <paolo@codedby.pm>
2020-02-08 14:51:46 +0100

    Implement all data source methdos

--- Sources/GenericDataSource/TableViewDataSource.swift
+++ Sources/GenericDataSource/TableViewDataSource.swift
@@ -10,6 +10,15 @@ import UIKit
 class TableViewDataSource<C: TableViewCellConfigurator>:
 NSObject, UITableViewDataSource {
 
+    var headerText: String?
+    var footerText: String?
+
+    var canEditRows: Bool = true
+    var canMoveRows: Bool = true
+
+    var onCommitEditing: ((UITableViewCell.EditingStyle, IndexPath) -> Void)?
+    var onMoveRow: ((IndexPath, IndexPath) -> Void)?
+
     private let model: [C.Model]
     private let configurator: C
 
@@ -19,10 +28,13 @@ NSObject, UITableViewDataSource {
         super.init()
     }
 
-    func tableView(
-        _ tableView: UITableView,
-        numberOfRowsInSection section: Int
-    ) -> Int {
+    // MARK: - Standard
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
         return model.count
     }
 
@@ -43,5 +55,59 @@ NSObject, UITableViewDataSource {
         configurator.configure(cell, with: cellModel)
 
         return cell
+    }
+}
+
+// MARK: - Header and Footer
+
+extension TableViewDataSource {
+
+    func tableView(_ : UITableView, titleForHeaderInSection: Int) -> String? {
+        return headerText
+    }
+
+    func tableView(_ : UITableView, titleForFooterInSection: Int) -> String? {
+        return footerText
+    }
+}
+
+// MARK: - Table editing
+
+extension TableViewDataSource {
+
+    func tableView(_: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return canEditRows
+    }
+
+    func tableView(_: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return canMoveRows
+    }
+
+    func tableView(
+        _: UITableView,
+        commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath
+    ) {
+        onCommitEditing?(editingStyle, indexPath)
+    }
+
+    func tableView(_: UITableView, moveRowAt from: IndexPath, to: IndexPath) {
+        onMoveRow?(from, to)
+    }
+}
+
+// MARK: - Table index title
+
+extension TableViewDataSource {
+
+    func sectionIndexTitles(for: UITableView) -> [String]? {
+        return nil
+    }
+
+    func tableView(_: UITableView,
+        sectionForSectionIndexTitle title: String,
+        at index: Int
+    ) -> Int {
+        return NSNotFound
     }
 }
